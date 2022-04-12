ea94521ac2950d02c7719320a4424d407e646454
SanjeevCA <12109@coderacademy.edu.au>
2022-04-12 13:09:35 +1000

    bundler and gem testing

---
+++ src/test.rb
@@ -0,0 +1,28 @@
+require 'ru_bee'
+require 'rword'
+
+print "Enter a word: "
+
+word = gets.chomp
+
+puts word.correct?
+
+# uses all the letters -> need to create function that iterates though different combinations
+# descrambler = WordScramble::Descrambler.new(word)
+# pp descrambler.matching_words
+
+i = 9
+
+while i >= 2
+    # Generate the words and display the longest ones
+    # Maybe randomly pick a long word to display?
+    best_words = Rword.generate(word, i, true)
+
+    if best_words.length > 0
+        pp best_words
+        break
+    else
+        i -= 1
+    end    
+end
+
